85a8b1ebe97b2803ba38d33f4454995ba1862aac
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-05-17 22:27:59 -0600

    Fix root routing by removing the public/index.html file and then patching the route for root to go to the plural context[s] controller.

--- config/routes.rb
+++ config/routes.rb
@@ -51,7 +51,7 @@ Attend::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'context#index'
+  root :to => 'contexts#index'
 
   # See how all your routes lay out with "rake routes"
 
