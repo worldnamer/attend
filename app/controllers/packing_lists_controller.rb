6d7a18f2829de3010a93977334266d565727e0cc
John LeBoeuf-Little <worldnamer@worldnamer.com>
2013-05-19 20:58:05 -0600

    Enable packing list name changing.

--- app/controllers/packing_lists_controller.rb
+++ app/controllers/packing_lists_controller.rb
@@ -1,6 +1,15 @@
 class PackingListsController < ApplicationController
   respond_to :html, :json
 
+  # GET /contexts/1/packing_lists
+  # GET /contexts/1/packing_lists.json
+  def show
+    @context = Context.find(params[:context_id])
+    @packing_list = @context.packing_lists.find(params[:id])
+
+    respond_with @packing_list
+  end
+
   # GET /contexts/1/packing_lists/new
   # GET /contexts/1/packing_lists/new.json
   def new
@@ -8,6 +17,23 @@ class PackingListsController < ApplicationController
     @packing_list = @context.packing_lists.build
 
     respond_with @packing_list
+  end
+
+  # PUT /contexts/1
+  # PUT /contexts/1.json
+  def update
+    @context = Context.find(params[:context_id])
+    @packing_list = @context.packing_lists.find(params[:id])
+
+    respond_to do |format|
+      if @packing_list.update_attributes(params[:packing_list])
+        format.html { redirect_to @packing_list, notice: 'Context was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @packing_list.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   # POST /contexts/1/packing_lists
@@ -18,11 +44,11 @@ class PackingListsController < ApplicationController
 
     respond_to do |format|
       if @packing_list.save
-        format.html { redirect_to @context, notice: 'Context was successfully created.' }
-        format.json { render json: @context, status: :created, location: @context }
+        format.html { redirect_to @context, notice: 'Packing List was successfully created.' }
+        format.json { render json: @packing_list, status: :created, location: @packing_list }
       else
         format.html { render action: "new" }
-        format.json { render json: @context.errors, status: :unprocessable_entity }
+        format.json { render json: @packing_list.errors, status: :unprocessable_entity }
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Attend::Application.routes.draw do
   resources :contexts, except: :edit do
-    resources :packing_lists, only: [:new, :create]
+    resources :packing_lists, only: [:new, :create, :show, :update]
   end
 
   root :to => 'contexts#index'
